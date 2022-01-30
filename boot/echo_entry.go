ad01e8142cf43b10f1a987397c7c2bb903305ad9
dongxuny <dongxuny@gmail.com>
2022-01-30 21:21:31 +0800

    Bump up rk-entry version to v1.0.10

--- boot/echo_entry.go
+++ boot/echo_entry.go
@@ -344,7 +344,7 @@ func (entry *EchoEntry) GetDescription() string {
 
 // Bootstrap EchoEntry.
 func (entry *EchoEntry) Bootstrap(ctx context.Context) {
-	event, logger := entry.logBasicInfo("Bootstrap")
+	event, logger := entry.logBasicInfo("Bootstrap", ctx)
 
 	// Is swagger enabled?
 	if entry.IsSwEnabled() {
@@ -426,7 +426,7 @@ func (entry *EchoEntry) Bootstrap(ctx context.Context) {
 
 // Interrupt EchoEntry.
 func (entry *EchoEntry) Interrupt(ctx context.Context) {
-	event, logger := entry.logBasicInfo("Interrupt")
+	event, logger := entry.logBasicInfo("Interrupt", ctx)
 
 	if entry.IsSwEnabled() {
 		// Interrupt swagger entry
@@ -553,14 +553,23 @@ func (entry *EchoEntry) IsStaticFileHandlerEnabled() bool {
 // ***************** Helper function *****************
 
 // Add basic fields into event.
-func (entry *EchoEntry) logBasicInfo(operation string) (rkquery.Event, *zap.Logger) {
+func (entry *EchoEntry) logBasicInfo(operation string, ctx context.Context) (rkquery.Event, *zap.Logger) {
 	event := entry.EventLoggerEntry.GetEventHelper().Start(
 		operation,
 		rkquery.WithEntryName(entry.GetName()),
 		rkquery.WithEntryType(entry.GetType()))
+
+	// extract eventId if exists
+	if val := ctx.Value("eventId"); val != nil {
+		if id, ok := val.(string); ok {
+			event.SetEventId(id)
+		}
+	}
+
 	logger := entry.ZapLoggerEntry.GetLogger().With(
 		zap.String("eventId", event.GetEventId()),
-		zap.String("entryName", entry.EntryName))
+		zap.String("entryName", entry.EntryName),
+		zap.String("entryType", entry.EntryType))
 
 	// add general info
 	event.AddPayloads(
